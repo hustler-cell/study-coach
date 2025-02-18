1adea5376228128c38b32a7110bfc6f6ca1eab0c
alokkumarmahanta007 <alokmahanta007@gmail.com>
2025-02-18 20:54:06 +0530

    Updated style

--- app/components/benefitRow.tsx
+++ app/components/benefitRow.tsx
@@ -14,7 +14,7 @@ interface BenefitsRowProps {
 
 export const BenefitsRow: React.FC<BenefitsRowProps> = ({ benefits }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full mt-10">
   {benefits.map((benefit, index) => (
     <div key={index} className="w-full  mx-auto">
       <BenefitCard
--- app/home-page/whyChooseUs.tsx
+++ app/home-page/whyChooseUs.tsx
@@ -66,7 +66,7 @@ const WhyChooseUs: React.FC = () => {
         initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.3, duration: 0.6 }}
-        className="w-full "
+        className="w-full px-[30px]"
       >
         <BenefitsRow benefits={benefitsData} />
       </motion.div>
